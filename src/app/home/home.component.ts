9945555feb33acfd7d33c6b5828043f50eacdb5e
Sankar <bskrao23@gmail.com>
2023-02-25 00:13:46 +0530

    Table Sort
    *Added sorting
    *Animations for the row enter and leave
    *Empty Pockets if user deletes all the data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ToasterService } from '../services/toaster.service';
+import {trigger, transition, style, animate} from '@angular/animations'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger("enterAnimation", [
+      transition(":enter", [
+        style({ transform: "translateY(100%)", opacity: 0 }),
+        animate(
+          "200ms",
+          style({
+            transform: "translateY(0)",
+            opacity: 1
+          })
+        )
+      ])
+    ]),
+    trigger("leaveAnimation", [
+      transition(":leave", [
+        style({ transform: "translateX(0)", opacity: 1 }),
+        animate("500ms", style({ transform: "translateX(100%)", opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -124,6 +145,25 @@ export class HomeComponent implements OnInit {
    ]
    masterCheckBox : boolean = false;
    isAtleastOneUserSelected : boolean = false;
+   isTable: boolean = true;
+   tableHeaders: TableHeader[] = [{
+    name : 'User Name',
+    propName : 'userName',
+    sortIcon: FlatSortIcon.none
+   },
+   {
+    name : 'Phone Number',
+    propName : 'phoneNumber',
+    sortIcon: FlatSortIcon.none
+   },
+   {
+    name : 'Email ID',
+    propName : 'emailID',
+    sortIcon: FlatSortIcon.none
+   }
+  ]
+
+
   constructor(private toaster: ToasterService) {}
 
   ngOnInit(): void {
@@ -136,6 +176,7 @@ export class HomeComponent implements OnInit {
       this.users.splice(this.users.findIndex(user=> user.id==item.id),1)
     })
     this.toaster.success("Records Deleted Successfully","Delete")
+    this.isAtleastOneUserSelected = false;
   }
 
   onMasterCheckBoxChange() {
@@ -154,6 +195,42 @@ export class HomeComponent implements OnInit {
   onUserSelectionChange() {
     this.isAtleastOneUserSelected = this.users.findIndex(user=> user.isSelected==true) >= 0 ? true : false;
   }
+
+  onColumnSortChange(currentHeader : TableHeader) {
+
+    this.isTable = false;
+    //Changing SortIcons for otherHeaders to default
+    this.tableHeaders.forEach(header=> {
+      if(header.name!=currentHeader.name) {
+        header.sortIcon = FlatSortIcon.none
+      }
+    })
+
+    //Changing SortIcon for currentHeader
+    if(currentHeader.sortIcon === FlatSortIcon.none) {
+      currentHeader.sortIcon = FlatSortIcon.ascending
+    }
+    else if(currentHeader.sortIcon===FlatSortIcon.ascending) {
+      currentHeader.sortIcon = FlatSortIcon.descending
+    }
+    else currentHeader.sortIcon = FlatSortIcon.ascending
+
+    //Actual Sorting based on currentHeader
+    const column = currentHeader.propName as keyof User
+    if(currentHeader.sortIcon==FlatSortIcon.ascending) {
+      this.users.sort((a,b)=>{
+        return a[column] > b[column] ? 1 : -1
+      })
+    }
+    else if(currentHeader.sortIcon==FlatSortIcon.descending) {
+      this.users.sort((a,b)=>{
+        return a[column] < b[column] ? 1 : -1
+      })
+    }
+    setTimeout(()=>{
+      this.isTable = true;
+    },1)
+  }
 }
 
 export interface User {
@@ -163,3 +240,16 @@ export interface User {
   phoneNumber : string
   emailID : string
 }
+
+export interface TableHeader {
+  name : string
+  propName: string
+  sortIcon: string
+}
+
+export enum FlatSortIcon {
+  none = 'fi fi-sr-sort',
+  ascending = 'fi fi-rr-sort-amount-down-alt',
+  descending = 'fi fi-rr-sort-amount-up-alt'
+}
+
